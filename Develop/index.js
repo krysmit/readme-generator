a269164e253ab5740513426de08c8f6a9ccde7f4
Krystyn Smith <krskrsmith@gmail.com>
2021-09-23 22:33:13 -0500

    set up inquirer and added the array of questions to be prompted

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,49 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your project:',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What are the steps required to install your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Describe how to use your application:',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which license did you use?',
+            choices: ['Creative Commons','MIT','Microsoft Public'],
+          },
+          {
+            type: 'input',
+            name: 'contribute',
+            message: 'Explain if/what others can contribute to your work:',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Provide an example of how to run the code:',
+        }
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,16 +1,16 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns a LICENSE BADGE based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
+// TODO: Create a function that returns the LICENSE LINK
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
+// TODO: Create a function that RETURNS LICENSE SECTION of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// TODO: Create a function to GENERATE MARKDOWN for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
